c1c7a17b23f0796ec5ebc72eacbcc4946cea0a87
Noan Felipe <noanfelipe@gmail.com>
2022-11-30 00:40:44 -0300

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 
 namespace API.DTOs
 {
@@ -10,6 +7,13 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required] public DateOnly? DateOfBirth { get; set; } // optional to make required work!
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
+
         [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
